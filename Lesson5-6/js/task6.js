3918ecef342e8c7c7058a0d8ed9c897b77567b35
Expatgit <m.kropp003@gmail.com>
2020-11-24 01:59:39 +0300

    кто молодец? Интернет - мололдец!

--- Lesson5-6/js/task6.js
+++ Lesson5-6/js/task6.js
@@ -11,35 +11,66 @@
         '        ];\n' +
         '        \n' +
         '        1. Увеличить кажый элемент массива на 10\n' +
-        '        2. Создать массив, в который войдут положительные элементы numsArr\n')
-    let numsArr = [
-        [3, 5, -1, 6, 0],
-        [56, -9, 111, 6],
-        [11, 86, -12],
-    ];
-    let newArr = [];
-    for (let i of numsArr) {
+        '        2. Создать массив, в который войдут положительные элементы numsArr\n\n')
+
+     let numsArr = [
+         [3, 5, -1, 6, 0],
+         [56, -9, 111, 6],
+         [11, 86, -12],
+     ];
+
+     let newArr = [];
+     for (let i of numsArr) {
 
 
-        for (let j in i) {
+         for (let j in i) {
 
-            newArr.push(i[j]);
+             newArr.push(i[j]);
+         }
+         /* console.log(newArr)*/
+     }
+
+     console.log('конвертнули трехмерный массив в одномерный');
+ /*
+      for (let k of newArr) {
+          k+=10;
+          numsArr.pop(newArr[k]);
+
+          console.log(numsArr);
+
+      }*/
+     console.log(typeof newArr[0]);
+     console.log(typeof numsArr[0]); //Какая прелесть! 8 часов впустую...) К объекту 10 не прибавляется...
+     numsArr = newArr;
+     newArr = newArr.map(function (digit) {
+         return digit + 10;
+     });
+
+     console.log(newArr);
+
+ /*    let numsArr2 = numsArr.map(function (digit) {
+        if (digit > 0) {
+            return digit;
         }
-       /* console.log(newArr)*/
-    }
 
-    console.log('конвертнули трехмерный массив в одномерный');
+     }); //мысль остановилась.... надо убрать und из консоли
 
-    for (let k of newArr) {
-        k+=10;
-        numsArr.pop(newArr[k]);
-
-        console.log(numsArr);
+        console.log(numsArr2);*/
+    for (let i = 0; i <numsArr.length; i++) {
+        if (numsArr[i] < 0) {                  // почему numsArr[i], а не просто i????
+            numsArr.splice(i, 1);
+        }
 
     }
-    console.log(newArr);
+   /* for (let i = numsArr.length; i > 0; i--) {
+        if (numsArr[i] < 0) {
+            numsArr.splice(i, 1);
+        }
+
+    }*/
     console.log(numsArr);
-}
 
+
+ }
 
 
